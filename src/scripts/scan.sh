cb22d02ee2078e49998c0b7c39a91f1d311cdda4
Bryan Johnson <d.bryan.johnson@gmail.com>
2024-10-23 22:06:06 -0400

    fix scan script

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -4,8 +4,9 @@ EXIF_CSV="/storage/exif.csv"
 TAGS_FILE=$(dirname "$(realpath "$0")")/tags.txt
 
 if [ -f ${EXIF_CSV} ]; then
-    find /photo -type f -newer ${EXIF_CSV} -exec \
-        exiftool -@ "${TAGS_FILE}" -fast -n -api "missingtagvalue^=" -csv {} \; \
+    # find /photo -type f -newer ${EXIF_CSV} -exec \
+    find /photo -exec \
+        exiftool -@ "${TAGS_FILE}" -i @eaDir -fast -n -api "missingtagvalue^=" -csv {} \; \
         | tail -n +2 >> "${EXIF_CSV}"
 else
     exiftool -@ "${TAGS_FILE}" -csv -i @eaDir -fast -progress -n -r /photo > "${EXIF_CSV}"
