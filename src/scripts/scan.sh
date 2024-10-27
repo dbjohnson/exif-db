8e0c56cac87d553d3dbc321bd3db9f8fa6b5cf28
Bryan Johnson <d.bryan.johnson@gmail.com>
2024-10-26 22:55:34 -0400

    force tags

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -5,8 +5,8 @@ TAGS_FILE=$(dirname "$(realpath "$0")")/tags.txt
 
 if [ -f ${EXIF_CSV} ]; then
     find /photo -not -path '*/[@.]*' -type f -newer ${EXIF_CSV} > tmp.txt && \
-        exiftool -@ "${TAGS_FILE}" -@ tmp.txt -fast -n -api "missingtagvalue^=" -csv \
+        exiftool -f -@ "${TAGS_FILE}" -@ tmp.txt -fast -n -api "missingtagvalue^=" -csv \
         | tail -n +2 >> "${EXIF_CSV}" && rm tmp.txt
 else
-    exiftool -@ "${TAGS_FILE}" -csv -i @eaDir -fast -progress -n -r /photo > "${EXIF_CSV}"
+    exiftool -f -@ "${TAGS_FILE}" -csv -i @eaDir -fast -progress -n -r /photo > "${EXIF_CSV}"
 fi
